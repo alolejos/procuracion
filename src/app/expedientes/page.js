419510dd70c3dbfc8ac6f8392536a9fdf8bbc621
alolejos <marcelogomezgrois@gmail.com>
2024-12-16 15:32:17 -0300

    apuntando backend a localhost

--- src/app/expedientes/page.js
+++ src/app/expedientes/page.js
@@ -15,7 +15,7 @@ export default function Expedientes() {
   useEffect(() => {
     const fetchExpedientes = async () => {
       try {
-        const response = await axios.get('http://15.228.73.54:3001/api/expedientes/', {
+        const response = await axios.get('http://localhost:3001/api/expedientes/', {
           headers: {
             Authorization: `${token}`
           }
